9f256b2a426df2a825f2ff424ea03a567839f5fe
Rahul <42063508+therahulbehera@users.noreply.github.com>
2026-01-07 14:37:13 +0530

    feat: add suspense and spinner for loading state in products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,18 @@
 import Products from "@/components/products";
 import ProductsFilter from "@/components/products-filter";
-import React from "react";
+import { Spinner } from "@/components/ui/spinner";
+import React, { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
 
 const page = () => {
   return (
     <div className="m-6">
       <ProductsFilter />
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 justify-center items-center md:mx-40">
-        <Products />
+        <Suspense fallback={<Spinner />}>
+          <Products />
+        </Suspense>
       </div>
     </div>
   );
